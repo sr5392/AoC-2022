5da57ab0d8f7f48daefb1e69b352c8088c1c0359
PurpleCloud <s.richter92@web.de>
2022-12-05 22:08:32 +0100

    Shorten conversion of elements

--- day4/part_1.js
+++ day4/part_1.js
@@ -5,13 +5,12 @@ fs.readFileSync("input.txt", "utf-8").split("\r\n").forEach((element) => {
     const group = element;
     const [sections1, sections2] = group.split(",");
 
-    const [sections1Low, sections1High] = sections1.split("-").map(element => Number(element));
-    const [sections2Low, sections2High] = sections2.split("-").map(element => Number(element));
+    const [sections1Low, sections1High] = sections1.split("-").map(Number);
+    const [sections2Low, sections2High] = sections2.split("-").map(Number);
 
     if((sections1Low >= sections2Low && sections1High <= sections2High) 
     || (sections2Low >= sections1Low && sections2High <= sections1High))
             count += 1;
-
 });
 
 console.log(count);
