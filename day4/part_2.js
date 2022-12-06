a7d40773a2677d45cc9e95a1e9430cb307833bdd
PurpleCloud <s.richter92@web.de>
2022-12-06 17:29:18 +0100

    Add day 4 part 2

---
+++ day4/part_2.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+
+let count = 0;
+fs.readFileSync("input.txt", "utf-8").split("\r\n").forEach((element) => {
+    const group = element;
+    const [sections1, sections2] = group.split(",");
+
+    const [sections1Low, sections1High] = sections1.split("-").map(Number);
+    const [sections2Low, sections2High] = sections2.split("-").map(Number);
+
+    if((sections1Low <= sections2High && sections1Low >= sections2Low)
+    || (sections2Low <= sections1High && sections2Low >= sections1Low)
+    || (sections1High >= sections2Low && sections1High <= sections2High)
+    || (sections2High >= sections1Low && sections2High <= sections1High)){
+        count += 1;
+    }    
+});
+
+console.log(count);
